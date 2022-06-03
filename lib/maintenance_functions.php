0721229b5cc065240fab50e7d03eebed5cf5dd74
Thomas Skerbis <thomas.skerbis@mac.com>
2022-06-03 12:44:48 +0200

    added get_host()

--- lib/maintenance_functions.php
+++ lib/maintenance_functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the maintenance package.
  *
@@ -8,23 +9,49 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-class maintenance_functions { 
-	
-	public function checkUrl($url) {
-		if ($url) {
-			if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) { 
-				return false;	
-			}
-			return true;
-		}
-	}
-	public function checkIp($ip) {
-		if($ip){
-			if (filter_var($ip, FILTER_VALIDATE_IP)) {
-	    		return true;
-			}
-			return false;
-		}
-	}
+
+class maintenance_functions
+{
+
+    public function get_host()
+    {
+        if ($host = $_SERVER['HTTP_X_FORWARDED_HOST']) {
+            $elements = explode(',', $host);
+
+            $host = trim(end($elements));
+        } else {
+            if (!$host = $_SERVER['HTTP_HOST']) {
+                if (!$host = $_SERVER['SERVER_NAME']) {
+                    $host = !empty($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '';
+                }
+            }
+        }
+
+        // Remove port number from host
+        $host = preg_replace('/:\d+$/', '', $host);
+
+        return trim($host);
+    }
+
+
+
+
+    public function checkUrl($url)
+    {
+        if ($url) {
+            if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+                return false;
+            }
+            return true;
+        }
+    }
+    public function checkIp($ip)
+    {
+        if ($ip) {
+            if (filter_var($ip, FILTER_VALIDATE_IP)) {
+                return true;
+            }
+            return false;
+        }
+    }
 }
-?>
